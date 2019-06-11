0b1da29894c0b00283ee8bcd9ee4a02d8a7dae18
LinHuijuan <1197310697@qq.com>
2019-06-11 23:02:04 +0800

    project init

--- vue-travel/src/pages/home/Home.vue
+++ vue-travel/src/pages/home/Home.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>Hello travel</div>
+    <div>
+        <div>Hello travel</div>
+        <router-link to='/list'>list</router-link>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Home'
+  name: 'Home'
 }
 </script>
 
--- vue-travel/src/router/index.js
+++ vue-travel/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: 'Home'
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
