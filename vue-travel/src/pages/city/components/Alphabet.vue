9b11b3fd47fb8bf1da9e4ffcdbdc092a725407c9
LinHuijuan <1197310697@qq.com>
2019-06-21 23:33:59 +0800

    change scss

--- vue-travel/src/pages/city/components/Alphabet.vue
+++ vue-travel/src/pages/city/components/Alphabet.vue
@@ -68,19 +68,21 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
-  .list
-    display: flex
-    flex-direction: column
-    justify-content: center
-    position: absolute
-    top: 1.58rem
-    right: 0
-    bottom: 0
-    width: .4rem
-    .item
-      line-height: .4rem
-      text-align: center
-      color: $bgColor
+<style lang="scss" scoped>
+  @import '~styles/varibles.scss';
+  .list{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    top: 1.58rem;
+    right: 0;
+    bottom: 0;
+    width: .4rem;
+    .item{
+      line-height: .4rem;
+      text-align: center;
+      color: $bgColor;
+    }
+  }
 </style>
