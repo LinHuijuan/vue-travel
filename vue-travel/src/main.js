6e6e8b2a0caea25226508290fbb9e8f674a6d7a1
LinHuijuan <1197310697@qq.com>
2019-06-23 00:19:33 +0800

    finish detail pages

--- vue-travel/src/main.js
+++ vue-travel/src/main.js
@@ -5,12 +5,16 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'babel-polyfill'
+import 'swiper/dist/css/swiper.css'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
